63c25dd175586b1b8cb76c98b173af7c903d8cc0
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-04 20:29:46 -0700

    refactored breedFetcher and added test file to test for invalid breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,26 @@
-const breedName = process.argv[2];
 const req = require("request");
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-req(url, (err, response, body) => {
-  if (err) {
-    console.log("Request faild", err);
-  }
-  const object = JSON.parse(body);
-  const breed = object[0];
-  // console.log(typeof breed);
-  if (!breed) {
-    return console.log(`${breedName} Not found!`);
-    // console.log("error: ", err);
-  }
-  console.log(breed.description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  req(url, (err, response, body) => {
+    if (err) {
+      return callback(err, null);
+    }
+    const object = JSON.parse(body);
+    const breed = object[0];
+    let desc;
+    if (!breed) {
+      desc = undefined;
+      return callback(err, desc);
+      // console.log("error: ", err);
+    } else {
+      desc = breed.description;
+      // console.log(typeof breed);
+      // return console.log(breed.description);
+
+      return callback(err, desc);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
